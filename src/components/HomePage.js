c53fd165c4af91737b53a378021f0da70da9f032
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-07 23:58:32 -0400

    Setup Css and Homepage views

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,28 @@
-import { useState , useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import React from 'react'
+
+import React from "react";
 
 function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <>
+      <div>
+        <h1>Jeopardy Trivia App</h1>
+        <h2>Score</h2>
+        <div>
+          <button>Increase</button>
+          <button>Decrease</button>
+          <button>Reset</button>
+        </div>
+        <h2>Let's Play!</h2>
+        <button>Get Question</button>
+        <h2>Category:</h2>
+        <h2>Points:</h2>
+        <h2>Answer:</h2>
+        <button>Click to Reveal Question</button>
+      </div>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
