7896017fe11e8b9b0cef5811690b294ad2fb02bd
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-08 14:19:04 -0400

    Commit map() return

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,42 +1,64 @@
 import { useState, useEffect } from "react";
 
-
 import React from "react";
 
 function HomePage() {
-    const [ question, setQuestion ] = useState([])
+  const [question, setQuestion] = useState([]);
 
-    const fetchData = async () => {
-        try {
-            const response = await fetch("https://jservice.io/api/random")
-            const data = await response.json()
-            setQuestion(data);
-        }catch( err ) {
-            console.error(err)
-        }
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://jservice.io/api/random");
+      const data = await response.json();
+      setQuestion(data);
+    } catch (err) {
+      console.error(err);
     }
+  };
 
-    useEffect(() =>{
-        fetchData()
-    }, []) //if effect doesn't need props or state
-    
+  useEffect(() => {
+    fetchData();
+  }, []); //if effect doesn't need props or state
+
+  const [hide, setHide] = useState(true);
+  const [score, setScore] = useState(0);
+
+  const increaseScore = (data) => {
+    setScore(score + parseInt(data));
+  };
+  const decreaseScore = (data) => {
+    setScore((score = parseInt(data)));
+  };
+  const toggleQuestion = (data) => {
+    setHide(data);
+  };
+  const resetScore = () => {
+    setScore(0);
+  };
+
   return (
     <>
-      <div>
-        <h1>Jeopardy Trivia App</h1>
-        <h2>Score</h2>
-        <div>
-          <button>Increase</button>
-          <button>Decrease</button>
-          <button>Reset</button>
-        </div>
-        <h2>Let's Play!</h2>
-        <button>Get Question</button>
-        <h2>Category:</h2>
-        <h2>Points:</h2>
-        <h2>Answer:</h2>
-        <button>Click to Reveal Question</button>
-      </div>
+    {question.map((item, idx)=>{
+        return (
+            <>
+              <div>
+                <h1>Jeopardy Trivia App</h1>
+                <h2>Score</h2>
+                <div>
+                  <button>Increase</button>
+                  <button>Decrease</button>
+                  <button>Reset</button>
+                </div>
+                <h2>Let's Play!</h2>
+                <button>Get Question</button>
+                <h2>Category:</h2>
+                <h2>Points:</h2>
+                <h2>Answer:</h2>
+                <button>Click to Reveal Question</button>
+              </div>
+            </>
+
+        )
+    })}
     </>
   );
 }
